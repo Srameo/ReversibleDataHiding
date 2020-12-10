d10edfab94f17f3a1577a48cad8e94d8e643d12e
czy1812977 <52919810+czy1812977@users.noreply.github.com>
2020-12-10 15:50:09 +0800

    debug
    remove the mistake of decryption

--- src/util/encrypt_util.py
+++ src/util/encrypt_util.py
@@ -4,6 +4,7 @@ import src.util.path_util as pu
 from src.encryption_2d_lscm.InitialStateGeneration import init_confusion_matrix, init_states
 from src.encryption_2d_lscm.diffusion import row_diffusion, column_diffusion
 from src.encryption_2d_lscm.permutation import permutation
+import src.receiver.decryption as dec
 
 SECRET_KEY = {
     "x0": [1] * 13 + [0] * 13 + [1] * 13 + [0] * 13,
@@ -23,14 +24,13 @@ def encrypt(P, S, F):
     :param F: pixel
     :return: 加密后的图片矩阵
     """
-    global T2
     M, N = P.shape
     init_matixs = init_confusion_matrix(M, N, init_states(S))
     for i in range(0, 4):
-        T = permutation(P, init_matixs[i], M, N)
-        T1 = row_diffusion(T, init_matixs[i], F, M, N)
-        T2 = column_diffusion(T1, init_matixs[i], F, M, N)
-    return T2
+        P = permutation(P, init_matixs[i], M, N)
+        P = row_diffusion(P, init_matixs[i], F, M, N)
+        P = column_diffusion(P, init_matixs[i], F, M, N)
+    return P
 
 
 if __name__ == '__main__':
@@ -39,3 +39,5 @@ if __name__ == '__main__':
     P = iu.read_img(file_path, iu.READ_GRAY)
     encrypted_img = encrypt(P, SECRET_KEY, 256).astype(np.uint8)
     iu.print_img(encrypted_img, "encrypted_lena")
+    anti_encrypted_img=dec.decryptioner(encrypted_img,SECRET_KEY, 256).astype(np.uint8)
+    iu.print_img(anti_encrypted_img, "anti-encrypted_lena")
